590f8285071404d6604b4c4c875241fcef182903
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-11-30 18:17:27 +0200

    Navbar and Hero section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,26 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav
+    variants={navVariants}
+    className={`${styles.xPaddings} py-8 relative`}
+    initial="hidden"
+    whileInView="show"
+  >
+    <div className="absolute inset-0 w-6/12 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img src="/search.svg" alt="search" className="w-6 h-6 object-contain" />
+      <h2 className="font-extrabold text-6 leading-[30px] text-white">
+        METAVERSUS
+      </h2>
+      <img src="/menu.svg" alt="menu" className="w-6 h-6 object-contain" />
+    </div>
+  </motion.nav>
 );
 
 export default Navbar;
